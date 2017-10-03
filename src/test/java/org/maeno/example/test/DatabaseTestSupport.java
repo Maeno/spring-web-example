9bcde86fc9ce2ffb2cdec795b51e3e0726e679fd
Maeno <developer.daisuke.maeno@gmail.com>
2017-10-03 22:25:49 +0900

    Add a project mapper and test case. (#9)

--- src/test/java/org/maeno/example/test/DatabaseTestSupport.java
+++ src/test/java/org/maeno/example/test/DatabaseTestSupport.java
@@ -28,10 +28,24 @@ public class DatabaseTestSupport {
                 "INSERT INTO ACCOUNT (USERNAME, EMAIL, PASSWORD) VALUES ('SCOTT', 'SCOTT@example.com', 'TIGER');",
                 "INSERT INTO ACCOUNT (USERNAME, EMAIL, PASSWORD) VALUES ('FOO', 'bar@example.com', 'BAR');")
                 .forEach(s -> jdbcTemplate.execute(s));
+
+        Arrays.asList(
+                "CREATE TABLE IF NOT EXISTS PROJECT (" +
+                        " ID INTEGER IDENTITY PRIMARY KEY, " +
+                        " NAME VARCHAR(20), " +
+                        " TYPE VARCHAR(20), " +
+                        " STARTDATE DATE, " +
+                        " VERSIONNO INTEGER " +
+                        ");",
+                "INSERT INTO PROJECT (NAME, TYPE, STARTDATE, VERSIONNO) VALUES ('ProA', 'PRIVATE', '2017-01-01', 0);",
+                "INSERT INTO PROJECT (NAME, TYPE, STARTDATE, VERSIONNO) VALUES ('ProB', 'PUBLIC', '2017-07-07', 0);"
+
+        ).forEach(s -> jdbcTemplate.execute(s));
     }
 
     @After
     public void tearDown() throws Exception {
         jdbcTemplate.execute("DROP TABLE ACCOUNT;");
+        jdbcTemplate.execute("DROP TABLE PROJECT;");
     }
 }
